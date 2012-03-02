fed659f5d80955fc6b2c4b5d6b667eca863e6f95
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-03-02 20:28:59 +0000

    Update Compress-Raw-Zlib to CPAN version 2.051
      [DELTA]
    
      2.051 20 February 2012
    
          * Bug in Compress::Raw::Zlib on Windows
            [#75222]
    
      2.050 20 February 2012
    
          * Build failure on Irix & Solaris.
            [RT #69985]
    
      2.049 18 February 2012
    
          * Include zlib 1.2.6 source.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -376,7 +376,7 @@ use File::Glob qw(:case);
 
     'Compress::Raw::Zlib' => {
         'MAINTAINER'   => 'pmqs',
-        'DISTRIBUTION' => 'PMQS/Compress-Raw-Zlib-2.048.tar.gz',
+        'DISTRIBUTION' => 'PMQS/Compress-Raw-Zlib-2.051.tar.gz',
 
         'FILES'    => q[cpan/Compress-Raw-Zlib],
         'EXCLUDED' => [
